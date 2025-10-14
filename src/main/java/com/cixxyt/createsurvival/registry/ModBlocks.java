5a3c4b9e7ea1fee7ea89975f54d781194200d049
cixxwastaken <117723267+cixxwastaken@users.noreply.github.com>
2025-10-15 00:00:59 +0100

    Working build with create implementation.

--- src/main/java/com/cixxyt/createsurvival/registry/ModBlocks.java
+++ src/main/java/com/cixxyt/createsurvival/registry/ModBlocks.java
@@ -1,4 +1,22 @@
-package com.cixxyt.createsurvival.registry;
-
-public class ModBlocks {
-}
+//package com.cixxyt.createsurvival.registry;
+//
+//import com.cixxyt.createsurvival.CreateSurvivalMod;
+//import net.minecraft.world.item.CreativeModeTab;
+//import net.minecraft.world.item.Item;
+//import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+//import net.minecraftforge.registries.DeferredRegister;
+//import net.minecraftforge.registries.ForgeRegistries;
+//import net.minecraftforge.registries.RegistryObject;
+//
+//public class ModBlocks {
+//
+//
+//
+//
+//
+//
+//    @SuppressWarnings("removal")
+//    public static void register() {
+//        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+//    }
+//}
